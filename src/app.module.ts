b0e34895acf48a64b4c832347076f83e5f3f00a6
Raja Usman Murad <usmankiyani501@gmail.com>
2024-04-29 16:20:39 +0500

    create the new module for list crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { UserModule } from './user/user.module';
+import { ListModule } from './list/list.module';
 
 @Module({
-  imports: [],
-  // imports: [UserModule],
+  imports: [ListModule],
   controllers: [AppController],
   providers: [AppService],
 })
