33cb91f4cfa493beb1d8cf7ce292babdf04f8e3a
Raja Usman Murad <usmankiyani501@gmail.com>
2024-04-29 16:20:39 +0500

    create the new module for list crud

---
+++ src/list/list.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+// import { ListController } from './list.controller';
+// import { ListService } from './list.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  // controllers: [ListController],
+  providers: [],
+  // providers: [ListService],
+})
+export class ListModule {}
