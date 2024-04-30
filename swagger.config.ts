44fff92b276af67f1cd2411a2a0179b43306c4b1
Raja Usman Murad <usmankiyani501@gmail.com>
2024-04-30 10:55:40 +0500

    Integrate the swagger with the app
    Create the swagger.config.json file and add configuration
    In main file integrate the swagger with the app
    In controller add the tag for module

---
+++ swagger.config.ts
@@ -0,0 +1,19 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle('List API')
+  .setDescription('API for list crud project')
+  .setVersion('1.0')
+  .addTag('list api')
+  // .addBearerAuth(
+  //   {
+  //     type: 'http',
+  //     scheme: 'bearer',
+  //     bearerFormat: 'JWT',
+  //     name: 'JWT',
+  //     description: 'Enter JWT token',
+  //     in: 'header',
+  //   },
+  //   'JWT-auth', // @ApiBearerAuth() in controller!
+  // )
+  .build();
