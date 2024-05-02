93ef698bf3ac8e7e4613bd69d2601a0a4977aa18
Raja Usman Murad <usmankiyani501@gmail.com>
2024-05-02 15:35:46 +0500

    Integrate the mongodb
    Install the npm package of mongoose using this command npm i
    @nestjs/mongoose mongoose
    Add the Mongoose module into the main file module and pass thw string to
    the mongoose module function

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ListModule } from './list/list.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ListModule],
+  imports: [
+    ListModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/listcrudnest'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
