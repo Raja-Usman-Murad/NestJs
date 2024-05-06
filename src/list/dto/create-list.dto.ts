0e51a9a770e8bcaa02528f0a325c789264f997ae
Raja Usman Murad <usmankiyani501@gmail.com>
2024-05-06 12:06:28 +0500

    Add the default values.

--- src/list/dto/create-list.dto.ts
+++ src/list/dto/create-list.dto.ts
@@ -1,23 +1,30 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+} from 'class-validator';
 import { MESSAGE } from 'utils/constants/response.message';
 
 // import config from '../../../config/config';
 
 export class CreateListDto {
-  @ApiProperty({ type: String })
+  @ApiProperty({ type: String, default: 'Match' })
   @IsNotEmpty({ message: `Title ${MESSAGE.MUST_BE_PROVIDED}` })
   @IsString({ message: `Title ${MESSAGE.MUST_BE_STRING}` })
   title: String;
 
-  @ApiProperty({ type: String })
+  @ApiProperty({ type: String, default: 'abc@gamil.com' })
   @IsNotEmpty({ message: `Email ${MESSAGE.MUST_BE_PROVIDED}` })
   @IsEmail({}, { message: `${MESSAGE.INVALID_EMAIL_FORMAT}` })
   email: string;
 
-  @ApiProperty({ type: String })
-  @IsNotEmpty({ message: `Password ${MESSAGE.MUST_BE_PROVIDED}` })
-  @IsString({ message: `Password ${MESSAGE.MUST_BE_STRING}` })
+  @ApiPropertyOptional({ type: String, default: 'Today is my _ match' })
+  // @IsOptional()
+  @IsNotEmpty({ message: `Description ${MESSAGE.MUST_BE_PROVIDED}` })
+  @IsString({ message: `Description ${MESSAGE.MUST_BE_STRING}` })
   // @Matches(config.regex.passwordRegex, {
   //   message: `${MESSAGE.PASSWORD_REQUIREMENTS}`,
   // })
